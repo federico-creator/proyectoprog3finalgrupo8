f4ad17a71a30258f0911bfd735ca97f11816e552
isaacyoon <iyoon@udesa.edu.ar>
2021-11-23 11:32:04 -0300

    h

--- src/screen/BusquedaPerfil.js
+++ src/screen/BusquedaPerfil.js
@@ -1,25 +1,68 @@
 import React, {Component} from 'react';
 import { StyleSheet,Text, View, TouchableOpacity, Image, FlatList, ActivityIndicator, TextInput } from 'react-native';
-
+import Posts from '../components/Posts';
+import {db} from "../firebase/config"
 class BusquedaPerfil extends Component{
 
     constructor(){
         super()
         this.state={
-            
+            perfil:"",
+            post: [],
+            buscado: false,
         }
     }
 
-    componentDidMount(){
+    showPost(){
+        
+            (db.collection("posts").where("user", "==", this.state.perfil).onSnapshot((docs)=>{
+                let posts = []
+                docs.forEach((doc)=>{
+                    posts.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                })
+                this.setState({
+                    post: posts
+                })
+            }))  
         
     }
 
+    
+    
+
     render(){
         return(
-        
-        
+            <View style={styles.container}>
+
             <Text> Esta es pagina para buscar perfil</Text>
 
+            <TextInput style={styles.input}
+                keyboardType="email-address"
+                placeholder="Email"
+                onChangeText={text => this.setState({perfil:text})}
+            />
+        
+
+            {this.state.perfil.length==0?
+                <TouchableOpacity style={styles.touchable2}>
+                    <Text style={styles.texto}>Buscar Post</Text>
+                </TouchableOpacity>:
+                <TouchableOpacity style={styles.touchable} onPress={()=> this.showPost()}>
+                    <Text style={styles.texto}>Buscar Post</Text>
+                </TouchableOpacity>
+            }
+            <FlatList  
+                    data={this.state.post}
+                    keyExtractor={(data)=> data.id}
+                    renderItem={(item)=>( <Posts data={item}/> )}  >
+            </FlatList>
+            {this.state.buscado==true && this.state.post.length==0? <Text>No existen publicaciones</Text>:<Text></Text>}
+            </View>
+
+            
         )
     }
    
